5462c0375de16159bfe6f57a2e313cd75eb499bd
OmarAdel898 <omar.97453@cs.mti.edu.eg>
2026-03-12 01:29:27 +0200

    Initial project structure for cafeteria system

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/public/css/style.css">
+</head>
+<body>
