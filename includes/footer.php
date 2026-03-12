8243a79a6934a9f8c5362d716e845178b131da11
AhmedSamehOOx7 <ahmedsameh300025@gmail.com>
2026-03-12 22:49:44 +0200

    Add New File AdminController

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= BASE_URL ?>/public/js/app.js"></script>
-</body>
-</html>
+    <!-- <script src="/public/js/app.js"></script> -->
+    </body>
+    <!--  BASE_URL  -->
+
+    </html>
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/public/css/style.css">
+    <!-- <link rel="stylesheet" href="  BASE_URL ?>/public/css/style.css"> -->
 </head>
+
 <body>
